8f2442d056a0caa4e79615e700d96e5634a500e3
Aaron Enyetu <aaronenyetu@gmail.com>
2022-08-15 16:48:21 -0700

    Test passing for creating a new user and added route

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,15 +1,32 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
+
+const newUser = {
+  email: 'test@example.com',
+  password: '123456'
+};
 
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+
+  it('#Post /api/v1/users, creates a new user', async () => {
+    const res = await request(app).post('/api/v1/users').send(newUser);
+    const { email } = newUser;
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual({
+      user: {
+        id: expect.any(String),
+        email
+      },
+      message: 'You are signed in!'
+    });
   });
+
+
   afterAll(() => {
     pool.end();
   });
