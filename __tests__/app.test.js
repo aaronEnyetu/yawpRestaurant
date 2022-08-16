2fc320a289ddca21a476129374a8383018354fdb
Aaron Enyetu <aaronenyetu@gmail.com>
2022-08-16 15:25:15 -0700

    Test passing for Post route to sign in an existing user

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,9 +4,9 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 const mockUser = {
-  username: 'mock', 
+  username: 'mock2', 
   password: '123456',
-  email: 'test@example.com'
+  email: 'test2@example.com'
 };
 
 
@@ -21,17 +21,34 @@ describe('backend-express-template routes', () => {
 
   it('#POST /user should create and sign in a new user', async () => {
     const res = await request(app).post('/api/v1/users').send(mockUser);
-   
+    console.log(res.body);
 
     expect(res.status).toBe(200);
     expect(res.body).toEqual({
       user: {
         id: expect.any(String),
-        email: 'test@example.com',
-        username: 'mock'
+        email: 'test2@example.com',
+        username: 'mock2'
       },
       message: 'successfully signed in!',
    
     });
   });
+
+  it('#POST /sign-in should sign in an existing user', async () => {
+    const response = await request(app).post('/api/v1/users/sessions').send({
+      username: 'mock1',
+      email: 'test1@example.com',
+      password: '123456',
+    });
+
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual({
+      message: 'successfully signed in'
+     
+    });
+  });
+
+ 
+
 });
--- sql/setup.sql
+++ sql/setup.sql
@@ -12,3 +12,9 @@ CREATE TABLE yawp_users (
     password_hash TEXT NOT NULL
 );
 
+INSERT INTO yawp_users
+(username, password_hash, email)
+VALUES
+('mock', '$2b$10$T09ZdD3tXIWXQynWmH0S.OHRefjmAFfV/.0aDx89hAvSB6/mf3COe', 'test@example.com'),
+('mock1', '$2b$10$pGvCZFNuI7Pg2KVMXoKJZeqLlc5GEuqjjZtzbf17PK104gleG5Txu', 'test1@example.com')
+;
