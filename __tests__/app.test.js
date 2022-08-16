d1f597b7e381a7b8adc2796c616eaad7a3e02bf9
Aaron Enyetu <aaronenyetu@gmail.com>
2022-08-16 14:17:39 -0700

    Made changes to the create user Post test. Test is passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,31 +3,35 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
-const newUser = {
-  email: 'test@example.com',
-  password: '123456'
+const mockUser = {
+  username: 'mock', 
+  password: '123456',
+  email: 'test@example.com'
 };
+
 
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
+  
+  afterAll(() => {
+    pool.end();
+  });
 
-  it('#Post /api/v1/users, creates a new user', async () => {
-    const res = await request(app).post('/api/v1/users').send(newUser);
-    const { email } = newUser;
+  it('#POST /user should create and sign in a new user', async () => {
+    const res = await request(app).post('/api/v1/users').send(mockUser);
+   
+
     expect(res.status).toBe(200);
     expect(res.body).toEqual({
       user: {
         id: expect.any(String),
-        email
+        email: 'test@example.com',
+        username: 'mock'
       },
-      message: 'You are signed in!'
+      message: 'successfully signed in!',
+   
     });
-  });
-
-
-  afterAll(() => {
-    pool.end();
   });
 });
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,13 +1,14 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
 
-DROP TABLE IF EXISTS yawp_users;
+DROP TABLE IF EXISTS yawp_users CASCADE;
 
 
 CREATE TABLE yawp_users (
 
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    username TEXT NOT NULL,
     email TEXT NOT NULL UNIQUE,
     password_hash TEXT NOT NULL
 );
+
