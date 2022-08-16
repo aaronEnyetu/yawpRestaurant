b5a86d7ccdd7389e3df448f62d9515542b2a011a
Aaron Enyetu <aaronenyetu@gmail.com>
2022-08-16 16:31:55 -0700

    Created restaurant table and reviews table

---
+++ __tests__/restaurants.test.js
@@ -0,0 +1,28 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+
+
+
+describe('backend-express-template routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+  
+  afterAll(() => {
+    pool.end();
+  });
+
+  it('#GET /restaurants should return a list of restaurants', async () => {
+    const response = await request(app).get('/api/v1/restaurants');
+
+    expect(response.status).toBe(200);
+    expect(response.body[0]).toEqual({
+      id: expect.any(String),
+      name: expect.any(String),
+    });
+  });
+
+});
--- sql/setup.sql
+++ sql/setup.sql
@@ -18,3 +18,40 @@ VALUES
 ('mock', '$2b$10$T09ZdD3tXIWXQynWmH0S.OHRefjmAFfV/.0aDx89hAvSB6/mf3COe', 'test@example.com'),
 ('mock1', '$2b$10$pGvCZFNuI7Pg2KVMXoKJZeqLlc5GEuqjjZtzbf17PK104gleG5Txu', 'test1@example.com')
 ;
+
+
+CREATE TABLE yawp_restaurants (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  name TEXT NOT NULL
+);
+
+INSERT INTO yawp_restaurants
+ (name)
+ VALUES 
+ ('Tasty'),
+ ('Yawp Foods'),
+ ('Taste Buds');
+
+ CREATE TABLE yawp_reviews (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  rest_id BIGINT NOT NULL,
+  user_id BIGINT NOT NULL,
+  review varchar(255) NOT NULL,
+  FOREIGN KEY (rest_id) REFERENCES yawp_restaurants(id),
+  FOREIGN KEY (user_id) REFERENCES yawp_users(id)
+);
+
+INSERT INTO yawp_reviews
+(
+    rest_id, 
+    user_id, 
+    review
+    )
+
+VALUES
+('1', '1', 'the best place in town'),
+('1', '1', 'amazing staff and tasty food'),
+('1', '2', 'the food was amazing'),
+('2', '2', 'You have to be here to believe it'),
+('2', '1', 'the chicken tikka was great'),
+('2', '2', 'I will definetly eat here again');
