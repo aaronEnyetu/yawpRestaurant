a18ccec3b665d055f6a8b4bcc6c429206f0e451e
Aaron Enyetu <aaronenyetu@gmail.com>
2022-08-17 16:12:13 -0700

    Test passing to delete a review based on user id. added routing

--- __tests__/reviews.js
+++ __tests__/reviews.js
@@ -26,4 +26,15 @@ describe('backend-express-template routes', () => {
     const review = await agent.post('/api/v1/restaurants/1/reviews').send({ stars: 5, detail: 'It was okay' });
     expect (review.status).toBe(200);
   });
+  it('#DELETE /api/v1/reviews/:id should delete a review based on user id', async () => {
+    const agent = request.agent(app);
+    await agent.post('/api/v1/users').send({ ...mockUser, email: 'admin@example.com' });
+  
+    const res = await agent.delete('/api/v1/reviews/1');
+    
+    expect(res.status).toBe(200);
+      
+  });
 });
+
+
